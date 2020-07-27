d1227642794341e530274ae7f4ac38d5252bfb44
Raghavendra Nataraj <nataraj.raghavendra@gmail.com>
2020-07-26 20:08:03 -0500

    actions

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@ make install
 cd ../NetCHASM
 mkdir build
 cd build
-if [ $1 = "build" ]; then
+if [ "$1" = "build" ]; then
     cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DINCLUDE_PATHS=/tmp/install/include -DLINK_PATHS=/tmp/install/lib64
     make build VERBOSE=1
 fi
 
-if [ $1 = "package" ] ; then
+if [ "$1" = "package" ]; then
     cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DINCLUDE_PATHS=/tmp/install/include -DLINK_PATHS=/tmp/install/lib64
     make package VERBOSE=1
 fi
